719bc2863da23507e44ef331f98bd3b525d068b9
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-01-26 14:34:09 +0000

    messages appearing in a list

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import os
 
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
+
+
+def add_messages(username, message):
+    """add messages to the messages list"""
+    messages.append("{}: {}".format(username, message))
+
+def get_all_messages():
+    """Get all messages and separate them with a `br`"""
+    return "<br>".join(messages)
+
+
 
 
 @app.route('/')
@@ -10,14 +22,18 @@ def index():
     """Main page with instructions"""
     return "to send a message use /USERNAME/MESSAGE"
 
+
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
-
+    """create new message and redirect to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 if __name__ == '__main__':
